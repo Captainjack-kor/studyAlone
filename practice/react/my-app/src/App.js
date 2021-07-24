c289b9ac02b6e0d0f15d30fa1490d4d1938900e5
Jack Lim <jennystr233@gmail.com>
2021-07-24 21:44:12 +0900

    react beginner update

--- practice/react/my-app/src/App.js
+++ practice/react/my-app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  firstName: 'Happy',
+  lastName: 'ending!!'
+};
+
+let getGreeting = (think) => {
+  if(think) {
+    return <h1>Mika {formName(user)}</h1>
+  }
+  return <h1> NOOOOOO </h1>
+}
+
+let formName = (user) => {
+  return <span>{user.firstName + ' ' + user.lastName}</span>;
+}
+
 function App() {
+  const predict = true;
+  const name = "REACT";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+      <div>
+        {getGreeting(predict)}
+        {formName(user)}
+      </div>
     </div>
   );
 }
