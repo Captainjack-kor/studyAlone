62208bdcf380a3877b5c176fe3b127027c8fbf6f
Captainjack-kor <jennystr233@gmail.com>
2021-10-06 23:35:38 +0900

    vue

---
+++ practice/Vue/vuestudy/study.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script>
+  // route 정의 , css 옵션 , background-size 차이, rem 이 무엇인가.
+  // login처리 , localstorage ? , beforeEach, some 이란
+</script>
+
+<style>
+
+</style>
