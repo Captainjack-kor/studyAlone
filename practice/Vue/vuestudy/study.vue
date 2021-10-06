e56e1a03bd4bcd170724e1ff196eb33e02f1ebdb
Captainjack-kor <jennystr233@gmail.com>
2021-10-07 00:09:04 +0900

    vue test02

--- practice/Vue/vuestudy/study.vue
+++ practice/Vue/vuestudy/study.vue
@@ -5,6 +5,17 @@
 <script>
   // route 정의 , css 옵션 , background-size 차이, rem 이 무엇인가.
   // login처리 , localstorage ? , beforeEach, some 이란
+
+  //브라우저 저장소의 종류
+  // 로컬스토리지, 세션스토리지, 인데스드 디비 ...etc
+  // 로컬스토리지와 세션 스토리지는 https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
+  // 에서 참고하기
+
+  //! 시나리오
+  //1. localStorage에 키와 값으로 로그인 정보를 저장한다.
+  //2. 로그인 정보가 있다면 새로 고침 해도 계속 로그인이 되어야 함.
+  //3. 로그아웃 누를 시 저장된 키 값을 모두 지운다.
+
 </script>
 
 <style>
