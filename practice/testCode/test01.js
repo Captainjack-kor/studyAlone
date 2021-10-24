44b52cd2aa2d38dba94d3c2dd4dca9f5f1d6200f
Captainjack-kor <jennystr233@gmail.com>
2021-10-24 12:44:23 +0900

    js test

--- practice/testCode/test01.js
+++ practice/testCode/test01.js
@@ -6,4 +6,18 @@ let obj = {};
 
 //어차피 배열안에는 이미 값이 다 들어가 있음...
 
-//
+/*
+  1. 값을 나눈다.
+  2. 
+
+*/
+let arr = [1,2,3,4,5]
+// let arrTest = [a,"b","c","d","e"];
+
+obj.a = arr[0];
+
+obj.b = arr[1];
+
+obj.c = arr[2];
+
+console.log(obj);
