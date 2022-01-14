a107db0cda59db3f24d5010a7fb0c905cec26380
Jack Lim <jennystr233@gmail.com>
2022-01-15 01:33:30 +0900

    merge

---
+++ practice/html/config.js
@@ -0,0 +1,4 @@
+// config.js
+var config = { // this should be const instead if you're using ES6 standards
+  apiKey : '123456789XYZ'
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+// const plus = (a, b) => a+b;
+// script_test
+
+// "dapi.kakao.com/v2/maps/sdk.js?appkey=123" 
+// export default {};
+
+document.getElementById("script_test").src = "";
