aa0f8aa6bd1c6aa7e9850a89382eb2ed9056747d
Captainjack-kor <jennystr233@gmail.com>
2022-02-22 16:58:41 +0900

    vuetify

--- practice/Vue/myapp/src/App.vue
+++ practice/Vue/myapp/src/App.vue
@@ -4,6 +4,74 @@
     <TodoInput v-on:addTodo="addTodo"></TodoInput>
     <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
     <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+    <div class="text-xs-center">
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        auto
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="indigo"
+            dark
+            v-on="on"
+          >
+            Menu as Popover
+          </v-btn>
+        </template>
+  
+        <v-card>
+          <v-list>
+            <v-list-tile avatar>
+              <v-list-tile-avatar>
+                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John">
+              </v-list-tile-avatar>
+  
+              <v-list-tile-content>
+                <v-list-tile-title>John Leider</v-list-tile-title>
+                <v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
+              </v-list-tile-content>
+  
+              <v-list-tile-action>
+                <v-btn
+                  :class="fav ? 'red--text' : ''"
+                  icon
+                  @click="fav = !fav"
+                >
+                  <v-icon>favorite</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+  
+          <v-divider></v-divider>
+  
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="message" color="purple"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Enable messages</v-list-tile-title>
+            </v-list-tile>
+  
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="hints" color="purple"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Enable hints</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+  
+            <v-btn flat @click="menu = false">Cancel</v-btn>
+            <v-btn color="primary" flat @click="menu = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-menu>
+    </div>
   </div>
 </template>
 
